839bce30682c5600ddfb4379187aee7954292f55
adhikasp <adhika.setyap@gmail.com>
2015-09-12 14:51:56 +0700

    Implement upgrade from Mega

--- converter/AreaConverter.java
+++ converter/AreaConverter.java
@@ -1,5 +1,5 @@
 /**
-* Program untuk mengkonversikan luas area dalam meter kubik menjadi hektar
+* Program untuk mengkonversikan luas area
 * Created by:
 * @author Maria Ana Megasari
 * @NIM 14/364598/TK/41980
@@ -10,14 +10,34 @@ import java.util.*; /* Digunakan untuk menggambi salag satu fungsi pada java yai
 
 public class AreaConverter implements Converter {
     public static void main(String[] args) {
-        float area; /* Mendeklarasikan variabel area yang bertipe data float */
-        Scanner luas = new Scanner(System.in); /* mendiskripsikan variabel scanner (luas) */
+        float km, hektar, dam, dm, cm, mm, panjang, lebar, luas; /*mendeskripsikan tipe data vaiabel yang ada*/
+        Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Masukkan Luas Dalam Meter Kubik"); /* Menampilkan Output */
-        area = luas.nextInt(); /* Menscan/membaca nilai yang yang dinputkan untuk di proses */
+        System.out.print("Masukkan panjang area (dalam meter) :"); /* Menampilkan Output */
+        panjang = keyboard.nextInt(); /* Menscan/membaca nilai yang yang dinputkan untuk di proses */
+        System.out.print("Masukkan lebar area (dalam meter) :");
+        lebar = keyboard.nextInt();
 
-        area = area / 10000; /* Operasi pengkonversian nilai yang diinputkan ke hektar*/
-        System.out.println("Luas Dalam Hektar = " + area); /*Menampilkan Output*/
+        luas = (panjang*lebar);/* Operasi untuk mencari luas area yang ingin dikonversikan*/
+        System.out.println("Luas area tersebut adalah (dalam m^2) :" +luas); /*menampilkan output luas area yang ingin dikonversikan*/
+
+        km = luas/1000000;
+        System.out.println("Luas area dalam km^2 :" +km ); /*menampilkan output hasil konversi ke km^2 */
+
+        hektar = luas/10000;
+        System.out.println("Luas area dalam hektar :" +hektar); /*menampilkan output hasil konversi ke hektar */
+
+        dam =  luas/100;
+        System.out.println("Luas area dalam dam^2 :" +dam); /*menampilkan output hasil konversi ke dam^2 */
+
+        dm = luas*100;
+        System.out.println("Luas area dalam dm^2 :" +dm); /*menampilkan output hasil konversi ke dm^2 */
+
+        cm = luas*10000;
+        System.out.println("Luas area dalam cm^2 :" +cm); /*menampilkan output hasil konversi ke cm^2 */
+
+        mm = luas*1000000;
+        System.out.println("Luas area dalam mm^2 :" +mm); /*menampilkan output hasil konversi ke mm^2 */
     }
 
     public void execute()
